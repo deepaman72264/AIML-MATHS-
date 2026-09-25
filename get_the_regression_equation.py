70a1badfb09e9db2233b6df43341e95c135acfff
deepaman7664 <deep.aman72264@gmail.com>
2026-09-25 22:05:54 +0530

    modified the data 2

--- get_the_regression_equation.py
+++ get_the_regression_equation.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 
 x=np.array([[1],[2],[3],[4],[5]])
-y=np.array([2,4,6,8,10])
+y=np.array([5,10,15,20,25])
 
 model=LinearRegression()
 model.fit(x,y)
